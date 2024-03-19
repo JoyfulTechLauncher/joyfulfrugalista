dfb1795c420bfb6a1ce9a1084f63efb3f1545aa4
Qingyang Liu <u7544859@anu.edu.au>
2024-03-20 02:38:28 +1100

    add login

--- screens/Login.js
+++ screens/Login.js
@@ -29,68 +29,80 @@ const Login = () => {
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>Sign in</Text>
       </TouchableOpacity>
+
       <TouchableOpacity>
         <Text style={styles.forgotPassword}>Forgot password</Text>
       </TouchableOpacity>
       <TouchableOpacity>
         <Text style={styles.signUp}>Sign up</Text>
       </TouchableOpacity>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  logoContainer: {
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 50,
-  },
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'stretch', // This will center the logo horizontally
+      padding: 20,
+      backgroundColor: '#fff',
+    },
+    logoContainer: {
+      width: 160,
+      height: 160,
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 50,
+      backgroundColor: '#E91E63',
+      borderRadius: 80,
+    },
   logoText: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#E91E63', // This should be the color of your logo
+    color: '#fff', // Logo text is probably white
   },
   title: {
-    fontSize: 20,
-    fontWeight: '500',
-    marginBottom: 10,
-    alignSelf: 'center',
+    fontSize: 26, // Slightly larger title
+    fontWeight: 'bold', // Bold title text
+    marginBottom: 30, // Increase spacing
+    alignSelf: 'left',
+    color: '#2d144b', // Assuming a dark title color
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    height: 50, // Larger touch area
+    borderColor: '#d3d3d3', // Lighter border color
     borderWidth: 1,
     marginBottom: 20,
-    paddingHorizontal: 10,
-    borderRadius: 5,
+    paddingHorizontal: 15, // More padding inside
+    borderRadius: 10, // Rounded borders
   },
   button: {
-    backgroundColor: '#673AB7',
-    paddingVertical: 10,
-    borderRadius: 5,
+    backgroundColor: '#673AB7', // Button background color
+    paddingVertical: 15, // Slightly larger button
+    borderRadius: 10, // Rounded borders
     alignItems: 'center',
+    marginBottom: 10, // Spacing after button
   },
   buttonText: {
-    color: '#fff',
+    color: '#fff', // Text color is white
     fontSize: 18,
   },
   forgotPassword: {
-    color: 'gray',
-    fontSize: 14,
+    color: '#673AB7', // Match sign-in button color
+    fontSize: 16, // Slightly larger font
     marginTop: 10,
+    alignSelf: 'center', // Center align text
   },
   signUp: {
-    color: 'gray',
-    fontSize: 14,
+    color: '#673AB7', // Match sign-in button color
+    fontSize: 16, // Slightly larger font
     marginTop: 10,
+    alignSelf: 'center', // Center align text
   },
 });
+
 
 export default Login;
