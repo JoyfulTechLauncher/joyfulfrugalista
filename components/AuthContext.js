7f335b3dd52cdb4d78440d986c6cbeba2f55ba3c
Xi <u7630433@anu.edu.au>
2024-04-28 04:05:37 +1000

    Refine AuthContext

---
+++ components/AuthContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext(null);
+export const useAuth = () => useContext(AuthContext);
+export const AuthProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+
+    // 提供设置用户的函数
+    const setUser = (user) => {
+        setCurrentUser(user);
+    };
+
+    return (
+        <AuthContext.Provider value={{ currentUser, setUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
